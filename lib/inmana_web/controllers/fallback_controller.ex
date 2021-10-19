e37d2585a42e36294386e6a2bbdf8d1088733638
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-10-19 18:24:38 -0300

    Create fallback controller

---
+++ lib/inmana_web/controllers/fallback_controller.ex
@@ -0,0 +1,12 @@
+defmodule InmanaWeb.FallbackController do
+    use InmanaWeb, :controller
+
+    alias InmanaWeb.ErrorView
+
+    def call(connection, {:error, %{result: result, status: status}}) do
+        connection
+        |> put_status(status)
+        |> put_view(ErrorView)
+        |> render("error.json", result: result)
+    end
+end
--- lib/inmana_web/controllers/restaurants_controller.ex
+++ lib/inmana_web/controllers/restaurants_controller.ex
@@ -4,6 +4,10 @@ defmodule InmanaWeb.RestaurantsController do
     alias Inmana.Restaurant
     alias Inmana.Restaurants.Create
 
+    alias InmanaWeb.FallbackController
+
+    action_fallback FallbackController
+
     def create(connection, params) do
         with {:ok, %Restaurant{} = restaurant} <- Create.call(params) do
             connection
