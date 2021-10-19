3f079bce8570a103b31763b3809518d59358c2d3
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-10-19 18:38:16 -0300

    Refactor to the facade design pattern

--- lib/inmana_web/controllers/restaurants_controller.ex
+++ lib/inmana_web/controllers/restaurants_controller.ex
@@ -2,14 +2,13 @@ defmodule InmanaWeb.RestaurantsController do
     use InmanaWeb, :controller
 
     alias Inmana.Restaurant
-    alias Inmana.Restaurants.Create
 
     alias InmanaWeb.FallbackController
 
     action_fallback FallbackController
 
     def create(connection, params) do
-        with {:ok, %Restaurant{} = restaurant} <- Create.call(params) do
+        with {:ok, %Restaurant{} = restaurant} <- Inmana.create_restaurant(params) do
             connection
             |> put_status(:created)
             |> render("create.json", restaurant: restaurant)
