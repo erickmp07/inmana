05d41943cd6f78937c478bf2bfe7e3044403f3b7
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-10-21 00:09:01 -0300

    Create delegate to show a supply

--- lib/inmana.ex
+++ lib/inmana.ex
@@ -1,8 +1,11 @@
 defmodule Inmana do
     alias Inmana.Restaurants.Create, as: RestaurantCreate
     alias Inmana.Supplies.Create, as: SupplyCreate
+    alias Inmana.Supplies.Get, as: SupplyGet
 
     defdelegate create_restaurant(params), to: RestaurantCreate, as: :call
 
     defdelegate create_supply(params), to: SupplyCreate, as: :call
+
+    defdelegate get_supply(params), to: SupplyGet, as: :call
 end
